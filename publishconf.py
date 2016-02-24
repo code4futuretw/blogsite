ed7964dd352a89bda007dfc088b0d1464d6d6d15
coursemdetw <course@mde.tw>
2016-02-24 21:02:54 +0800

    add disqus

--- publishconf.py
+++ publishconf.py
@@ -31,7 +31,7 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-DISQUS_SITENAME = "code4futureblog"
+DISQUS_SITENAME = "code4future"
 #GOOGLE_ANALYTICS = ""
 
 # 設定網誌以 md 檔案建立的 file system date 為準, 無需自行設定
